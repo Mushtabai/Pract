49cdf7798ee52d20d8e74aaacfb4fd725f180722
unknown <ivan_kravchenko_02@bk.ru>
2023-09-09 10:36:20 +0300

    New module

--- pract1/src/ru/mirea/it/ivbo01/Main.java
+++ pract1/src/ru/mirea/it/ivbo01/Main.java
@@ -9,6 +9,7 @@ public class Main {
     }
     public static void main(String[] args) {
 
+
         int[] midsum =  {1,2,5,6,3,7};
         int sum = 0;
         for(int i = 0; i!= midsum.length;i++){
